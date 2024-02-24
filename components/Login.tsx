a5568c71ff72fd3054d53ed98b49ac19ed927de4
shubham-gravitii <shubham@gravitii.xyz>
2024-02-25 03:05:01 +0530

    addItems component added

--- components/Login.tsx
+++ components/Login.tsx
@@ -4,15 +4,17 @@ import { useState } from "react";
 import "./Login.css";
 
 import bgSignIn from "../assests/login.svg"
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SignIn() {
 
-
+  const [isLoading, setisLoading] = useState(false)
   const [formState, setFormState] = useState({ email: "", password: "" });
   // eslint-disable-next-line
 
-  const handleChange = (event:any) => {
+  const handleChange = (event: any) => {
     const { name, value } = event.target;
 
     setFormState({
@@ -20,89 +22,106 @@ export default function SignIn() {
       [name]: value,
     });
   };
-  const handleFormSubmit = async (event:any) => {
+  const handleFormSubmit = async (event: any) => {
+
     event.preventDefault();
+    setisLoading(true)
+    toast("Success")
     // setisLoading(true);
-    
+
 
     //   Auth.login(data.login.token);
 
     //   setnotificationMsg("Login Successfull");
-      
-    // setisLoading(false);
 
-    setFormState({
-      email: "",
-      password: "",
-    });
+    // setisLoading(false);
+    setisLoading(false)
   };
   return (
-    <div className="loginContainer flex justify-center items-center">
-      <img src={bgSignIn.src} alt="" className="bgLoginImg"  />
-      <div className="formLogin bg-white flex flex-col justify-center  ">
-        <div className=" formLoginInside  w-full p-[20px] m-auto  rounded-md shadow-md lg:max-w-xl">
-          <h1 className="text-3xl font-semibold text-center text-purple-700 ">
-            Sign in
-          </h1>
-          <form className="mt-6">
-            <div className="mb-2">
-              <label
-                htmlFor="email"
-                className="block text-md font-semibold text-black"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                value={formState.email}
-                onChange={handleChange}
-                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div className="mb-2">
-              <label
-                htmlFor="password"
-                className="block text-md font-semibold text-white"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                value={formState.password}
-                onChange={handleChange}
-                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <a
-              href="/login"
-              className="text-xs text-purple-600 hover:underline"
-            >
-              Forget Password?
-            </a>
-            <div className="mt-6">
-              <button
-                onClick={handleFormSubmit}
-                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-              >
-                Login
-              </button>
-            </div>
-          </form>
+    <>
+      {isLoading ? <div className="parentLoader">
+        <span className="loader"></span>
+      </div> :
+        <div className="loginContainer flex justify-center items-center">
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
 
-          <p className="mt-8 text-xs font-light text-center text-black">
-            {" "}
-            Don't have an account?{" "}
-            <a
-              href="/signup"
-              className="font-medium text-purple-600 hover:underline"
-            >
-              Sign up
-            </a>
-          </p>
+          />
+          <img src={bgSignIn.src} alt="" className="bgLoginImg" />
+          <div className="formLogin bg-white flex flex-col justify-center  ">
+            <div className=" formLoginInside  w-full p-[20px] m-auto  rounded-md shadow-md lg:max-w-xl">
+              <h1 className="text-3xl font-semibold text-center text-purple-700 ">
+                Sign in
+              </h1>
+              <form className="mt-6">
+                <div className="mb-2">
+                  <label
+                    htmlFor="email"
+                    className="block text-md font-semibold text-black"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+                <div className="mb-2">
+                  <label
+                    htmlFor="password"
+                    className="block text-md font-semibold text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+                <a
+                  href="/login"
+                  className="text-xs text-purple-600 hover:underline"
+                >
+                  Forget Password?
+                </a>
+                <div className="mt-6">
+                  <button
+                    onClick={handleFormSubmit}
+                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-8 text-xs font-light text-center text-black">
+                {" "}
+                Don't have an account?{" "}
+                <a
+                  href="/signup"
+                  className="font-medium text-purple-600 hover:underline"
+                >
+                  Sign up
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+        }</>
   );
 }
--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -2,6 +2,8 @@
 import { useState, useContext, useEffect } from "react";
 import bgSignIn from "@/assests/login.svg";
 import { Checkbox } from "@/components/ui/checkbox";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // import { Link, useNavigate } from "react-router-dom";
 // import { useMutation } from "@apollo/client";
@@ -12,6 +14,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import "./SignUp.css";
 
 export default function SignUp() {
+  const [isLoading, setisLoading] = useState(false)
   const states = [
     { name: "state", label: "Andhra Pradesh", value: "Andhra Pradesh" },
     { name: "state", label: "Arunachal Pradesh", value: "Arunachal Pradesh" },
@@ -61,7 +64,7 @@ export default function SignUp() {
   });
 
   // update state based on form input changes
-  const handleChange = (event:any) => {
+  const handleChange = (event: any) => {
     const { name, value } = event.target;
     setFormState({
       ...formState,
@@ -69,148 +72,158 @@ export default function SignUp() {
     });
   };
 
-  
+
   // submit form
-  const handleFormSubmit = async (event:any) => {
+  const handleFormSubmit = async (event: any) => {
     event.preventDefault();
-    // setisLoading(true);
-    // use try/catch instead of promises to handle errors
-    try {
-      //   const { data } = await addUser({
-      //     variables: { ...formState },
-      //   });
-      //   setnotificationMsg("Sign Up Successfull");
-      //   navigate("/calculator");
-    } catch (e) {
-      //   setnotificationMsg(e.message);
-      //   console.error(e.message);
-    }
-    // setisLoading(false);
+    setisLoading(true);
+    // toast("success")
+    setisLoading(false);
   };
-  
+
 
   return (
-    <div className="loginContainer flex justify-center items-center">
-      <img src={bgSignIn.src} alt="" className="bgLoginImg" />
-      
-      <div className="bg-white p-5 formSignUp  flex flex-col   overflow-hidden  ">
-        <div>
-          <h3 className="text-3xl font-semibold text-center text-purple-700">
-            Sign Up
-          </h3>
-        </div>
-        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-[#efefef] shadow-md sm:max-w-lg sm:rounded-lg">
-          <form className="p-3">
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-md font-medium text-gray-700 undefined"
-              >
-                Full name
-              </label>
-              <div className="flex flex-col items-start">
-                
-                <input
-                  placeholder="Enter your Full Name"
-                  required={true}
-                  type="text"
-                  name="name"
-                  value={formState.name}
-                  onChange={handleChange}
-                  className="p-2 bg-[#f9f9f9] block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
-                />
-              </div>
-            </div>
-            <div className="w-full max-w-xs">
-            <label
-                htmlFor="name"
-                className="block text-md font-medium text-gray-700 undefined"
-              >
-                State
-              </label>
-              <select
-                value={formState.state}
-                onChange={handleChange}
-                name="state"
-                className="block w-full px-4 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              >
-                <option value="">Select a state</option>
-                {states.map((state, index) => (
-                  <option key={index} value={state.value}>
-                    {state.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="mt-4">
-              <label
-                htmlFor="email"
-                className="block text-md font-medium text-gray-700 undefined"
-              >
-                Email
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  placeholder="Enter your email"
-                  required={true}
-                  type="email"
-                  name="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                  className="p-2 bg-[#f9f9f9] block w-full mt-1 border-gray-500 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label
-                htmlFor="password"
-                className="block text-md font-medium text-gray-700 undefined"
-              >
-                Password
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  placeholder="Enter your password"
-                  required={true}
-                  type={showPassword ? "text" : "password"}
-                  onChange={handleChange}
-                  value={formState.password}
-                  name="password"
-                  className="p-2 block w-full bg-[#f9f9f9] mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
-                />
-                <div className="flex justify-center items-center ">
-                  <div className="m-2 text-black-500">Show Password</div>
-                  <Checkbox
-                    onClick={() => {
-                      setshowPassword(!showPassword);
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
+    <>
+      {isLoading ?
+        <div className="parentLoader">
+          <span className="loader"></span>
+        </div> :
+        <div className="loginContainer flex justify-center items-center">
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
 
-            <div className="flex items-center mt-4">
-              <button
-                onClick={handleFormSubmit}
-                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-              >
-                Register
-              </button>
+          />
+          <img src={bgSignIn.src} alt="" className="bgLoginImg" />
+
+          <div className="bg-white p-5 formSignUp  flex flex-col   overflow-hidden  ">
+            <div>
+              <h3 className="text-3xl font-semibold text-center text-purple-700">
+                Sign Up
+              </h3>
             </div>
-          </form>
-          <div className="mt-4 text-black">
-            Already have an account?{" "}
-            <span>
-              <a
-                className="text-purple-600 hover:underline cursor-pointer"
-                href="/login"
-              >
-                Log in
-              </a>
-            </span>
+            <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-[#efefef] shadow-md sm:max-w-lg sm:rounded-lg">
+              <form className="p-3">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-md font-medium text-gray-700 undefined"
+                  >
+                    Full name
+                  </label>
+                  <div className="flex flex-col items-start">
+
+                    <input
+                      placeholder="Enter your Full Name"
+                      required={true}
+                      type="text"
+                      name="name"
+                      value={formState.name}
+                      onChange={handleChange}
+                      className="p-2 bg-[#f9f9f9] block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
+                    />
+                  </div>
+                </div>
+                <div className="w-full max-w-xs">
+                  <label
+                    htmlFor="name"
+                    className="block text-md font-medium text-gray-700 undefined"
+                  >
+                    State
+                  </label>
+                  <select
+                    value={formState.state}
+                    onChange={handleChange}
+                    name="state"
+                    className="block w-full px-4 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  >
+                    <option value="">Select a state</option>
+                    {states.map((state, index) => (
+                      <option key={index} value={state.value}>
+                        {state.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="mt-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-md font-medium text-gray-700 undefined"
+                  >
+                    Email
+                  </label>
+                  <div className="flex flex-col items-start">
+                    <input
+                      placeholder="Enter your email"
+                      required={true}
+                      type="email"
+                      name="email"
+                      value={formState.email}
+                      onChange={handleChange}
+                      className="p-2 bg-[#f9f9f9] block w-full mt-1 border-gray-500 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
+                    />
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <label
+                    htmlFor="password"
+                    className="block text-md font-medium text-gray-700 undefined"
+                  >
+                    Password
+                  </label>
+                  <div className="flex flex-col items-start">
+                    <input
+                      placeholder="Enter your password"
+                      required={true}
+                      type={showPassword ? "text" : "password"}
+                      onChange={handleChange}
+                      value={formState.password}
+                      name="password"
+                      className="p-2 block w-full bg-[#f9f9f9] mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
+                    />
+                    <div className="flex justify-center items-center ">
+                      <div className="m-2 text-black-500">Show Password</div>
+                      <Checkbox
+                        onClick={() => {
+                          setshowPassword(!showPassword);
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex items-center mt-4">
+                  <button
+                    onClick={handleFormSubmit}
+                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+                  >
+                    Register
+                  </button>
+                </div>
+              </form>
+              <div className="mt-4 text-black">
+                Already have an account?{" "}
+                <span>
+                  <a
+                    className="text-purple-600 hover:underline cursor-pointer"
+                    href="/login"
+                  >
+                    Log in
+                  </a>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   );
 }
